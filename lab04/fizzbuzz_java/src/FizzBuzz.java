3733f0da45acc67cbe4377af6a51afec58f3cec6
Yvan04 <fpong073@uottawa.ca>
2022-06-23 16:36:04 -0400

    ...

---
+++ lab04/fizzbuzz_java/src/FizzBuzz.java
@@ -0,0 +1,17 @@
+package src;
+
+public class FizzBuzz{
+    public static String fizzbuzz(int n){
+
+        if(n%3 == 0 && n%5== 0){
+            return("FizzBuzz");
+
+        }else if (n%3==0){
+            return("Fizz");
+        }else if (n%5==0){
+            return("Buzz");
+        }else{
+            return(Integer.toString(n));
+        }
+    }
+}
---
+++ lab04/fizzbuzz_java/test/FizzBuzzTest.java
@@ -0,0 +1,14 @@
+package test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import src.FizzBuzz;
+
+class FizzBuzzTest {
+    @Test
+    void test1(){
+      assertEquals("fizz", FizzBuzz.fizzbuzz(1));
+    }
+}
